8a0ada3f040160710d4c2587834b784ba5f5dbe2
John Harvey <thegradientvector@gmail.com>
2013-02-12 22:17:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0363610afda9dd2025590beb213b1a5978c0820cc5bf6189dfd24ca7adcfbf67fb4a5ced816f3bb917045ad25f72eece9e21a355aa929ead9d7c0da2ad2f03d3'
