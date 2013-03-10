632d48423feaf811f97c927c92dfdd2001044cf1
John Harvey <thegradientvector@gmail.com>
2013-03-10 12:35:06 -0500

    Updated rspec tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,10 +14,15 @@ describe "UserPages" do
 	describe "profile page" do
 		# Code to make a user variable
 		let(:user) { FactoryGirl.create(:user) }
+		
 		before { visit user_path(user) }
 		
 		it { should have_selector('h1',		text: user.name) }
 		it { should have_selector('title', 	text: user.name) }
+		
+		describe "on navigation after initial submission" do
+			it { should_not have_selector("div.alert.alert-success", text: "Welcome to the Sample App!") }
+		end
 	end
 	
 	describe "signup" do
@@ -26,17 +31,39 @@ describe "UserPages" do
 		let(:submit) { "Create my account" }
 		
 		describe "with invalid information" do
+		
+			describe "after submission" do
+				before { click_button submit }
+				
+				it { should have_selector("title", text: "Sign up") }
+				it { should have_content("error") }
+			end
+			
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
 		end
 		
 		describe "with valid information" do
+			let(:name) { "Example User" }
+			let(:email) { "user@example.com" }
+			let(:password) { "foobar" }
+		
 			before do
-				fill_in "Name", with: "Example User"
-				fill_in "Email", with: "user@example.com"
-				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
+				fill_in "Name", with: name
+				fill_in "Email", with: email
+				fill_in "Password", with: password
+				fill_in "Confirmation", with: password
+			end
+			
+			describe "after saving the user" do
+				before { click_button submit }
+				
+				let(:user) { User.find_by_email(email) }
+				
+				it { should have_selector("title", text: name) }
+				it { should have_selector("div.alert.alert-success", text: "Welcome to the Sample App!") }
+				it { should have_selector("img", alt: name, class: "gravatar") }
 			end
 			
 			it "should create a new user" do
