8c03c1d4ea67c0affe899dee01aa0b4cbba45ec3
John Harvey <thegradientvector@gmail.com>
2013-03-13 23:34:08 -0500

    Refactored Rspecs to make use of more utility methods.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,21 +7,20 @@ describe "UserPages" do
 	describe "signup page" do
 		before { visit signup_path }
 		
-		it { should have_selector("h1",		text: "Sign up") }
-		it { should have_selector("title", 	text: full_title("Sign up")) }
+		it { should have_title("Sign up") }
+		it { should have_header("Sign up") }
 	end
 	
 	describe "profile page" do
-		# Code to make a user variable
 		let(:user) { FactoryGirl.create(:user) }
 		
 		before { visit user_path(user) }
 		
-		it { should have_selector('h1',		text: user.name) }
-		it { should have_selector('title', 	text: user.name) }
+		it { should have_title(user.name) }
+		it { should have_header(user.name) }
 		
 		describe "on navigation after initial submission" do
-			it { should_not have_selector("div.alert.alert-success", text: "Welcome to the Sample App!") }
+			it { should_not have_success_message("Welcome to the Sample App!") }
 		end
 	end
 	
@@ -35,7 +34,7 @@ describe "UserPages" do
 			describe "after submission" do
 				before { click_button submit }
 				
-				it { should have_selector("title", text: "Sign up") }
+				it { should have_title("Sign up") }
 				it { should have_content("error") }
 			end
 			
@@ -45,16 +44,9 @@ describe "UserPages" do
 		end
 		
 		describe "with valid information" do
-			let(:name) { "Example User" }
-			let(:email) { "user@example.com" }
-			let(:password) { "foobar" }
+			let(:user) { FactoryGirl.build(:user) }
 		
-			before do
-				fill_in "Name", with: name
-				fill_in "Email", with: email
-				fill_in "Password", with: password
-				fill_in "Confirmation", with: password
-			end
+			before { provide_valid_signup_information(user) }
 		
 			it "should create a new user" do
 				expect { click_button submit }.to change(User, :count).by(1)
@@ -62,11 +54,10 @@ describe "UserPages" do
 			
 			describe "after saving the user" do
 				before { click_button submit }
-				let(:user) { User.find_by_email(email) }
 			
-				it { should have_selector("title", text: user.name) }
-				it { should have_selector("div.alert.alert-success", text: "Welcome to the Sample App!") }
-				it { should have_selector("img", alt: name, class: "gravatar") }
+				it { should have_title(user.name) }
+				it { should have_success_message("Welcome to the Sample App!") }
+				it { should have_selector("img", alt: user.name, class: "gravatar") }
 				it { should have_link("Sign out") }
 			end
 		end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,10 +9,35 @@ include ApplicationHelper
 	# end
 # end
 
+def provide_valid_signup_information(user)
+	fill_in "Name", with: user.name
+	fill_in "Email", with: user.email
+	fill_in "Password", with: user.password
+	fill_in "Confirmation", with: user.password
+end
+
 def valid_signin(user)
 	fill_in "Email", with: user.email.upcase
 	fill_in "Password", with: user.password
 	click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_header do |header|
+	match do |page|
+		page.should have_selector('h1', text: header)
+	end
+end
+
+RSpec::Matchers.define :have_title do |title|
+	match do |page|
+		page.should have_selector('title', text: title)
+	end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector("div.alert.alert-success", text: message)
+	end
 end
 
 RSpec::Matchers.define :have_error_message do |message|
